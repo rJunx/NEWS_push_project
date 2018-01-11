85de6c4d29cafcd8466f7a4ab78bf41e1f6ed2d1
WeiChien Hsu <weichien711@gmail.com>
2018-01-11 11:18:47 -0600

    :rocket: Finish Scroll

--- index.js
+++
@@ -1,5 +0,0 @@
-import React from 'react';
-import ReactDOM form 'react-dom';
-import App from './App/App';
-
-ReactDOM.render{}
--- web_server/client/src/NewsPanel/NewsPanel.js
+++ web_server/client/src/NewsPanel/NewsPanel.js
@@ -8,63 +8,63 @@ class NewsPanel extends React.Component {
     this.state = { news: null };
   }
 
-  componentDidMount () {
+  handleScroll() {
+    const scrollY = window.scrollY
+        || window.pageYOffset
+        || document.documentElement.scrollYTop;
+    if ((window.innerHeight + scrollY) >= (document.body.offsetHeight - 50)) {
+      console.log('Loading more news!');
+      this.loadMoreNews();
+    }
+  }
+
+  componentDidMount() {
     this.loadMoreNews();
+    window.addEventListener('scroll', () => this.handleScroll());
   }
 
   loadMoreNews() {
-    this.setState({
-      news : [
-        {'url':'http://us.cnn.com/2017/02/15/politics/andrew-puzder-failed-nomination/index.html',
-         'title':"Inside Andrew Puzder's failed nomination",
-         'description':"In the end, Andrew Puzder had too much baggage -- both personal and professional -- to be confirmed as President Donald Trump's Cabinet.",
-         'source':'cnn',
-         'urlToImage':'http://i2.cdn.cnn.com/cnnnext/dam/assets/170215162504-puzder-trump-file-super-tease.jpg',
-         'digest':'3RjuEomJo26O1syZbU7OHA==\n',
-         'reason':'Recommend'
-        },
-        {'title': 'Zero Motorcycles CTO Abe Askenazi on the future of two-wheeled EVs',
-         'description': "Electric cars and buses have already begun to take over the world, but the motorcycle industry has been much slower to put out all-electric and hybrid models...",
-         'url': "https://techcrunch.com/2017/03/23/zero-motorcycles-cto-abe-askenazi-on-the-future-of-two-wheeled-evs/",
-         'urlToImage': "https://tctechcrunch2011.files.wordpress.com/2017/03/screen-shot-2017-03-23-at-14-04-01.png?w=764&h=400&crop=1",
-         'source': 'techcrunch',
-         'digest':"3RjuEomJo26O1syZbUdOHA==\n",
-         'time':"Today",
-         'reason':"Hot"
-       }]
+    const news_url = 'http://' + window.location.hostname + ':3000' + '/news';
+    const request = new Request(news_url, {method:'GET', cache:false});
 
-       });
+    fetch(request)
+      .then(res => res.json())
+      .then(news => {
+        this.setState({
+          news: this.state.news ? this.state.news.concat(news) : news,
+        });
+      });
   }
 
   renderNews() {
     const news_list = this.state.news.map(news => {
-      return (
-        <a className = 'list-group-item' key = {news.digest} href = '#'>
-          <NewsCard news = {news} />
-        </a>  
+      return(
+        <a className='list-group-item' key={news.digest} href="#">
+          <NewsCard news={news} />
+        </a>
       );
     });
 
-    return (
-      <div className = 'container-fluid'>
-        <div className = "list-group">
+    return(
+      <div className="container-fluid">
+        <div className="list-group">
           {news_list}
-        </div>  
+        </div>
       </div>
     );
   }
 
   render() {
     if (this.state.news) {
-      return (
+      return(
         <div>
           {this.renderNews()}
         </div>
       );
     } else {
-      return (
+      return(
         <div>
-          Loading ...
+          Loading...
         </div>
       );
     }
