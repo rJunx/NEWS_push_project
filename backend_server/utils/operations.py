3e889e7cff160d132ffaf8a06ae43e53cc07c2f1
WeiChien Hsu <weichien711@gmail.com>
2018-01-14 12:16:29 -0600

    :construction: Refactor-Put the news function into opearation.py

---
+++ backend_server/utils/operations.py
@@ -0,0 +1,16 @@
+import os
+import sys
+import json
+from bson.json_util import dumps
+
+# import utils packages
+sys.path.append(os.path.join(os.path.dirname(__file__), 'utils'))
+
+import mongodb_client
+
+NEWS_TABLE_NAME = "news"
+
+def getOneNews():
+    db = mongodb_client.get_db()
+    news = db[NEWS_TABLE_NAME].find_one()
+    return json.loads(dumps(news))
