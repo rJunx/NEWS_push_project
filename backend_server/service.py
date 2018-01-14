43b116a7c76f742919d513ec285c6001698787ee
WeiChien Hsu <weichien711@gmail.com>
2018-01-14 12:05:31 -0600

    :construction: Pylint check

--- backend_server/service.py
+++ backend_server/service.py
@@ -1,3 +1,4 @@
+"""Backend Server"""
 import json
 import os
 import sys
@@ -7,24 +8,27 @@ from bson.json_util import dumps
 
 # import utils packages
 sys.path.append(os.path.join(os.path.dirname(__file__), 'utils'))
-import mongodb_client
 
+import mongodb_client #pylint: disable = import-error, wrong-import-position
 
-SERVER_HOST = 'localhost';
-SERVER_PORT = 4040;
+SERVER_HOST = 'localhost'
+SERVER_PORT = 4040
 
-def add(a, b):
-  print("Add is called with %d and %d " %(a, b))
-  return a + b
+def add(num1, num2):
+    """Test Method"""
+    print("Add is called with %d and %d " %(num1, num2))
+    return num1 + num2
 
 def get_one_news():
-  print("get_one_news is called.")
-  news = mongodb_client.get_db()['news'].find_one()
-  return json.loads(dumps(news))
+    """Get News Method"""
+    print("get_one_news is called.")
+    news = mongodb_client.get_db()['news'].find_one()
+    return json.loads(dumps(news))
 
 RPC_SERVER = SimpleJSONRPCServer((SERVER_HOST, SERVER_PORT))
 RPC_SERVER.register_function(add, 'add')
 RPC_SERVER.register_function(get_one_news, 'get_one_news')
+
 print("Starting RPC server")
 
 RPC_SERVER.serve_forever()
