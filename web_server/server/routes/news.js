cd7effcfba64f5d7bd8ec4bf8bd82b81f0bc8328
WeiChien Hsu <weichien711@gmail.com>
2018-01-26 13:25:15 -0600

    :construction: Change Router from nodeJs backend connecting to backend service

--- web_server/server/routes/news.js
+++ web_server/server/routes/news.js
@@ -15,4 +15,13 @@ router.get('/userId/:userId/pageNum/:pageNum', function(req, res, next) {
   });
 });
 
+router.post('/userId/:userId/newsId/:newsId', function(req, res, next) {
+  console.log('Logging news click...');
+  user_id = req.params['userId'];
+  newsId = req.params['newsId'];
+
+  rpc_client.logNewsClickForUser(user_id, newsId);
+  res.status(200);
+});
+
 module.exports = router;
