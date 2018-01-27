54f122f6c6612ba4aa031bb5b00dbb1d9d37c6b4
WeiChien Hsu <weichien711@gmail.com>
2018-01-27 15:15:54 -0600

    :rocket: Finished LoginPage

--- web_server/client/src/index.js
+++ web_server/client/src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App/App';
+import LoginPage from './Login/LoginPage';
 
 ReactDOM.render(
-  <App />,
+  <LoginPage />,
   document.getElementById('root')
 );
